147e3deb82ef19564f57a2ddfdb53f4497c18553
Pedro Botelho <114412781+pedropibotelho@users.noreply.github.com>
2024-11-18 22:28:08 -0300

    coloquei as cores para usar no projeto

--- lib/themes/light_mode.dart
+++ lib/themes/light_mode.dart
@@ -2,14 +2,29 @@ import 'package:flutter/material.dart';
 
 ThemeData lightMode = ThemeData(
   colorScheme: const ColorScheme.light(
-    surface:  Color.fromARGB(255, 255, 255, 255), // Fundo branco
-    primary:  Color.fromARGB(255, 6, 145, 20), 
-    secondary:  Color.fromARGB(255, 245, 245, 245), // Cinza muito claro
-    tertiary:  Color.fromARGB(255, 240, 240, 240), // Um pouco mais escuro que o secundário
-    inversePrimary: Colors.black, // Texto escuro para contraste
+    surface: Color(0xFFFFFFFF), //fundo
+    primary: Color(0xFF38B887), //cor principal
+    secondary: Color.fromARGB(255, 245, 245, 245), //cor secundária
+    tertiary: Color.fromARGB(255, 240, 240, 240), //cor terciária
+    inversePrimary: Colors.black, //cor do texto
   ),
   cardTheme: const CardTheme(
-    color: Color.fromARGB(255, 255, 255, 255), // Cartões brancos
+    color: Color(0xFF127351),
     elevation: 4,
   ),
 );
+
+/*
+Cores que estão no PDF:
+Principais
+Verde Claro: #38B887
+Verde Escuro: #127351
+Branco: #FFFFFF
+Cores de Apoio:
+Cinza Escuro: #5B7275
+Azul Claro: #BCE4ED
+
+Fontes:
+Maven Pro
+Daincing Script
+*/
