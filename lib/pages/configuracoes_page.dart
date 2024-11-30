d193cfe199ef076f79615e45e38de2dff427121c
Sammy <134229270+SammyCanti@users.noreply.github.com>
2024-11-30 19:38:17 -0300

    so apra testes

---
+++ lib/pages/configuracoes_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class ConfiguracoesPage extends StatefulWidget {
+  const ConfiguracoesPage({super.key});
+
+  @override
+  State<ConfiguracoesPage> createState() => _ConfiguracoesPageState();
+}
+
+class _ConfiguracoesPageState extends State<ConfiguracoesPage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
