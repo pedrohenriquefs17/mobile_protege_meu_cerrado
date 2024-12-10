537df847a1f7764f5bb7b231b2bb611f7f7c40b5
Sammy <134229270+SammyCanti@users.noreply.github.com>
2024-12-10 09:55:03 -0300

    fix: i changed some things in the boarding page to make it look batter

--- lib/pages/on_boarding_page.dart
+++ lib/pages/on_boarding_page.dart
@@ -1,12 +1,11 @@
 import 'package:flutter/material.dart';
-import 'package:mobile_protege_meu_cerrado/components/app_large_text.dart';
 import 'package:mobile_protege_meu_cerrado/components/app_text.dart';
-import 'package:mobile_protege_meu_cerrado/components/pulsing_button.dart';
 import 'package:mobile_protege_meu_cerrado/components/responsive_button_cadastro.dart';
 import 'package:mobile_protege_meu_cerrado/components/responsive_button_login.dart';
 import 'package:mobile_protege_meu_cerrado/themes/theme_provider.dart';
 import 'package:provider/provider.dart';
 import 'package:shared_preferences/shared_preferences.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 class OnboardingPage extends StatefulWidget {
   const OnboardingPage({super.key});
@@ -16,8 +15,8 @@ class OnboardingPage extends StatefulWidget {
 }
 
 class _OnboardingPageState extends State<OnboardingPage> {
-  // Inicializando o PageController
   final PageController _pageController = PageController();
+  int _currentPage = 0;
 
   Future<void> _entrarSemLogar(BuildContext context) async {
     final prefs = await SharedPreferences.getInstance();
@@ -29,28 +28,22 @@ class _OnboardingPageState extends State<OnboardingPage> {
     });
   }
 
-  List images = [
-    "onBoardingF1.png",
-    "onBoardingF2.png",
-    "onBoardingF3.png",
+  final List<String> images = [
+    "primeira1.svg",
+    "segunda2.svg",
+    "terceira3.svg",
   ];
 
-  List text = [
-    "Inovação",
-    "Desenvolvimento",
-    "Transformação",
+  final List<String> titles = [
+    "Inovação para o nosso Cerrado",
+    "Preserve o Meio Ambiente",
+    "Conecte-se com a Natureza",
   ];
 
-  List text2 = [
-    "para o nosso Cerrado",
-    "do nosso Meio Ambiente",
-    "Conecte-se com a natureza",
-  ];
-
-  List text3 = [
-    "Preserve e inove com tecnologia.",
-    "Conecte-se ao meio ambiente.",
-    "Transforme sua relação com o Cerrado."
+  final List<String> subtitles = [
+    "Explore como a tecnologia pode ajudar a proteger o Cerrado.",
+    "Seja parte de uma revolução ambiental com inovação e cuidado.",
+    "Transforme seu impacto no meio ambiente com nossa comunidade.",
   ];
 
   @override
@@ -59,165 +52,169 @@ class _OnboardingPageState extends State<OnboardingPage> {
     final screenHeight = MediaQuery.of(context).size.height;
 
     return Scaffold(
-      body: PageView.builder(
-        controller: _pageController,
-        scrollDirection: Axis.horizontal,
-        itemCount: images.length,
-        itemBuilder: (_, index) {
-          return Stack(
-            children: [
-              Positioned(
-                bottom: 0,
-                left: 0,
-                right: 0,
-                child: index != images.length - 1
-                    ? Image.asset(
-                        "assets/images/" + images[index],
-                        fit: BoxFit.cover,
-                      )
-                    : SizedBox.shrink(),
-              ),
+      body: Stack(
+        children: [
+          PageView.builder(
+            controller: _pageController,
+            itemCount: images.length,
+            onPageChanged: (index) {
+              setState(() {
+                _currentPage = index;
+              });
+            },
+            itemBuilder: (_, index) {
+              return Stack(
+                children: [
+                  _buildBackground(images[index]),
+                  _buildContent(screenHeight, themeProvider, index),
+                  if (index != images.length - 1)
+                    _buildNextButton(screenHeight),
+                  if (index == images.length - 1)
+                    _buildFinalButtons(screenHeight),
+                ],
+              );
+            },
+          ),
+          _buildIndicators(screenHeight),
+        ],
+      ),
+    );
+  }
 
-              Positioned(
-                bottom: screenHeight * 0.1,
-                left: 0,
-                right: 0,
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: List.generate(
-                    images.length,
-                    (indicatorIndex) => Container(
-                      margin: const EdgeInsets.symmetric(horizontal: 4),
-                      width: indicatorIndex == index ? 16 : 8,
-                      height: 8,
-                      decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(8),
-                        color: indicatorIndex == index
-                            ? themeProvider.themeData.colorScheme.primary
-                            : Colors.grey,
-                      ),
-                    ),
-                  ),
-                ),
-              ),
+  Widget _buildBackground(String imagePath) {
+    return Positioned(
+      top: 50, // A imagem ficará 50 pixels abaixo do topo
+      left: 0,
+      right: 0,
+      child: SvgPicture.asset(
+        "assets/images/$imagePath",
+        fit: BoxFit.contain,
+        height: 335,
+        width: 335,
+      ),
+    );
+  }
 
-              // Conteúdo no topo
-              Positioned(
-                top: screenHeight * 0.1,
-                left: 20,
-                right: 20,
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    AppLargeText(
-                      text: text[index],
-                      color: themeProvider
-                              .themeData.textTheme.displayLarge?.color ??
-                          Colors.black,
-                    ),
-                    AppText(
-                      text: text2[index],
-                      size: 30,
-                      color: themeProvider
-                              .themeData.textTheme.displayLarge?.color ??
-                          Colors.black,
-                    ),
-                    SizedBox(height: 20),
-                    AppText(
-                      text: text3[index],
-                      color:
-                          themeProvider.themeData.textTheme.bodyLarge?.color ??
-                              Colors.black,
-                      size: 18,
-                    ),
-                    SizedBox(height: 40),
-                    if (index == 0)
-                      AppText(
-                        text:
-                            "O nosso app visa proteger o meio ambiente do Cerrado com o uso de tecnologia.",
-                        color: themeProvider
-                                .themeData.textTheme.bodyMedium?.color ??
-                            Colors.black,
-                        size: 16,
-                      ),
-                    if (index == 1)
-                      AppText(
-                        text:
-                            "Através de inovações, buscamos transformar como as pessoas interagem com a natureza.",
-                        color: themeProvider
-                                .themeData.textTheme.bodyMedium?.color ??
-                            Colors.black,
-                        size: 16,
-                      ),
-                    if (index == 2)
-                      AppText(
-                        text:
-                            "Junte-se a nós para um futuro mais sustentável e conectado com o meio ambiente.",
-                        color: themeProvider
-                                .themeData.textTheme.bodyMedium?.color ??
-                            Colors.black,
-                        size: 16,
-                      ),
-                  ],
-                ),
-              ),
+  Widget _buildContent(
+      double screenHeight, ThemeProvider themeProvider, int index) {
+    return Positioned(
+      top: screenHeight * 0.55,
+      left: 20,
+      right: 20,
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          AppText(
+            text: titles[index],
+            size: 28,
+            color: themeProvider.themeData.textTheme.displayLarge?.color ??
+                Colors.black,
+            textAlign: TextAlign.center,
+          ),
+          const SizedBox(height: 16),
+          AppText(
+            text: subtitles[index],
+            size: 16,
+            color: themeProvider.themeData.textTheme.bodyMedium?.color ??
+                Colors.black,
+            textAlign: TextAlign.center,
+          ),
+        ],
+      ),
+    );
+  }
 
-              //navegação (setinha)
-              if (index != images.length - 1)
-                Flexible(
-                  child: Positioned(
-                    bottom: screenHeight * 0.02,
-                    left: 0,
-                    right: 0,
-                    child: Align(
-                      alignment: Alignment.center,
-                      child: PulsingButton(
-                        onPressed: () {
-                          _pageController.nextPage(
-                            duration: Duration(milliseconds: 300),
-                            curve: Curves.easeInOut,
-                          );
-                        },
-                        child: Icon(
-                          Icons.arrow_forward_ios,
-                          color: themeProvider.themeData.colorScheme.secondary,
-                          size: 40,
-                        ),
-                      ),
-                    ),
-                  ),
-                ),
-              Positioned(
-                bottom: 150,
-                left: 0,
-                right: 0,
-                child: index == images.length - 1
-                    ? Column(
-                        mainAxisSize: MainAxisSize.min,
-                        children: [
-                          Row(
-                            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                            children: [
-                              ResponsiveButtonLogin(width: 140),
-                              ResponsiveButtonCadastro(width: 140),
-                            ],
-                          ),
-                          SizedBox(height: 20),
-                          Center(
-                            child: ElevatedButton(
-                              onPressed: () {
-                                _entrarSemLogar(context);
-                              },
-                              child: Text('Entrar sem logar'),
-                            ), // Novo botão
-                          ),
-                        ],
-                      )
-                    : SizedBox.shrink(),
-              )
-            ],
+  Widget _buildIndicators(double screenHeight) {
+    return Positioned(
+      bottom: screenHeight * 0.11,
+      left: 25,
+      right: 0,
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.start,
+        children: List.generate(
+          images.length,
+          (index) => AnimatedContainer(
+            duration: const Duration(milliseconds: 300),
+            margin: const EdgeInsets.symmetric(horizontal: 4),
+            width: _currentPage == index ? 16 : 8,
+            height: 8,
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(8),
+              color: _currentPage == index
+                  ? const Color(0xFF38B887)
+                  : Colors.grey.shade400,
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget _buildNextButton(double screenHeight) {
+    return Positioned(
+      bottom: screenHeight * 0.08,
+      right: 25,
+      child: TweenAnimationBuilder<double>(
+        tween: Tween<double>(begin: 1.0, end: 1.2),
+        duration: const Duration(seconds: 1),
+        curve: Curves.easeInOut,
+        builder: (context, scale, child) {
+          return Transform.scale(
+            scale: scale,
+            child: ElevatedButton(
+              style: ElevatedButton.styleFrom(
+                shape: const CircleBorder(),
+                backgroundColor: const Color(0xFF38B887),
+                padding: const EdgeInsets.all(16),
+              ),
+              onPressed: () {
+                _pageController.nextPage(
+                  duration: const Duration(milliseconds: 300),
+                  curve: Curves.easeInOut,
+                );
+              },
+              child: const Icon(
+                Icons.arrow_forward_ios,
+                color: Colors.white,
+              ),
+            ),
           );
         },
+        onEnd: () {
+          setState(() {});
+        },
+      ),
+    );
+  }
+
+  Widget _buildFinalButtons(double screenHeight) {
+    return Positioned(
+      bottom: 70,
+      left: 20,
+      right: 20,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              ResponsiveButtonLogin(width: 140),
+              ResponsiveButtonCadastro(width: 140),
+            ],
+          ),
+          const SizedBox(height: 20),
+          ElevatedButton(
+            style: ElevatedButton.styleFrom(
+              backgroundColor: const Color(0xFF38B887),
+              padding: const EdgeInsets.symmetric(horizontal: 24, vertical: 12),
+            ),
+            onPressed: () => _entrarSemLogar(context),
+            child: const Text(
+              "Entrar sem logar",
+              style: TextStyle(fontSize: 16, color: Colors.white),
+            ),
+          ),
+        ],
       ),
     );
   }
