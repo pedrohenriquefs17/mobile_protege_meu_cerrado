dd31e1b53d3f52aebec980074dec94e7d5c0bd08
Pedro Botelho <p2pinheiro2@gmail.com>
2024-12-09 22:19:36 -0300

    feat: começo de implementação da tela de configurações

--- lib/pages/config_page..dart
+++ lib/pages/config_page..dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:mobile_protege_meu_cerrado/components/responsive_button_cadastro.dart';
 import 'package:mobile_protege_meu_cerrado/components/responsive_button_login.dart';
+import 'package:mobile_protege_meu_cerrado/pages/on_boarding_page.dart';
 import 'package:mobile_protege_meu_cerrado/themes/theme_provider.dart';
 import 'package:provider/provider.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class ConfiguracoesPage extends StatefulWidget {
   const ConfiguracoesPage({super.key});
@@ -12,6 +14,28 @@ class ConfiguracoesPage extends StatefulWidget {
 }
 
 class _ConfiguracoesPageState extends State<ConfiguracoesPage> {
+  bool isLogado = false;
+
+  @override
+  initState() {
+    super.initState();
+    _estaLogado();
+  }
+
+  Future<void> _estaLogado() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    int? idUsuario = prefs.getInt('idUsuario');
+    if (idUsuario != null && idUsuario > 0) {
+      setState(() {
+        isLogado = true;
+      });
+    } else {
+      setState(() {
+        isLogado = false;
+      });
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     final themeProvider = Provider.of<ThemeProvider>(context);
@@ -21,22 +45,56 @@ class _ConfiguracoesPageState extends State<ConfiguracoesPage> {
       ),
       body: Column(
         children: [
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Text('Não Possui Login ?'),
-          ),
-          Column(
-            mainAxisSize: MainAxisSize.min,
-            children: [
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: [
-                  ResponsiveButtonLogin(width: 140),
-                  ResponsiveButtonCadastro(width: 140),
-                ],
-              ),
-            ],
-          )
+          if (isLogado) ...[
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Text('Você está logado'),
+            ),
+            Column(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    ElevatedButton.icon(
+                      onPressed: () => Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) => const OnboardingPage(),
+                        ),
+                      ),
+                      icon: const Icon(Icons.logout),
+                      label: const Text(
+                        'Sair da Conta',
+                      ),
+                      style: ElevatedButton.styleFrom(
+                        padding: const EdgeInsets.symmetric(
+                            horizontal: 24, vertical: 12),
+                        elevation: 8,
+                      ),
+                    ),
+                  ],
+                ),
+              ],
+            )
+          ] else ...[
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Text('Não Possui Login ?'),
+            ),
+            Column(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    ResponsiveButtonLogin(width: 140),
+                    ResponsiveButtonCadastro(width: 140),
+                  ],
+                ),
+              ],
+            )
+          ]
         ],
       ),
     );
